8c1d73b02c613959cfbe610e16f0ba60852ab499
Chenyu Wang <wangaaron331@gmail.com>
2023-12-19 10:18:19 -0500

    Initial Commit

---
+++ Memorize_game/Memorize_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_gameApp.swift
+//  Memorize_game
+//
+//  Created by Chenyu Wang on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memorize_gameUITests/Memorize_gameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Memorize_gameUITestsLaunchTests.swift
+//  Memorize_gameUITests
+//
+//  Created by Chenyu Wang on 12/19/23.
+//
+
+import XCTest
+
+final class Memorize_gameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
