06bb68f0e0fb5fb51b4e648088f501383e1d0520
Chenyu Wang <wangaaron331@gmail.com>
2023-12-19 22:09:36 -0500

    initial commit

--- Memorize_game/ContentView.swift
+++ Memorize_game/ContentView.swift
@@ -9,15 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 6)
+                    .strokeBorder(lineWidth: 3)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 6)
+            }
+            
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
